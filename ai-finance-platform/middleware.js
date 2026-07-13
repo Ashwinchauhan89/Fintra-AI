cdd3dabff386520d3d55fb8bfb98de65a3ba53d4
Ashwin Chauhan <ashwinchauhan99408@gmail.com>
2026-07-13 16:56:09 +0530

    fix db

--- ai-finance-platform/middleware.js
+++ ai-finance-platform/middleware.js
@@ -46,9 +46,9 @@ export default createMiddleware(aj, clerk);
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)|api/inngest).*)",
     // Always run for API routes
-    "/(api|trpc)(.*)",
+    "/(api(?!/inngest)|trpc)(.*)",
     "/__clerk/:path*",
   ],
 };
