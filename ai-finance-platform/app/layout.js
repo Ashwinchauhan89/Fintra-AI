adb361e21cf7094b950920ea150e30ed067d590a
Ashwin Chauhan <ashwinchauhan99408@gmail.com>
2026-07-13 11:50:33 +0530

    finally push after 1 year

---
+++ ai-finance-platform/app/layout.js
@@ -0,0 +1,43 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Fintra - AI",
+  description: "One stop Finance Platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo-sm.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <ClerkProvider>
+            <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+
+            <footer className="bg-blue-50 py-12 dark:bg-background border-t">
+              <div className="container mx-auto px-4 text-center text-gray-600 dark:text-gray-400">
+                <p>Made with 💗 by Ashwin Chauhan</p>
+              </div>
+            </footer>
+            </ThemeProvider>
+          </ClerkProvider>
+        </body>
+      </html>
+  );
+}
